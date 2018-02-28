7d35b8b3b6cd97006331a358d50a62cce1d00e07
Jake Fruci <jfruch629@gmail.com>
2018-02-27 20:14:55 -0500

    Revert "Add album model"

--- db/migrate/20180227135039_add_starting_year_to_albums.rb
+++
@@ -1,5 +0,0 @@
-class AddStartingYearToAlbums < ActiveRecord::Migration[5.1]
-  def change
-    add_column :albums, :starting_year, :string, null: false
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-Album.create!(title: "Innerspeaker", artist: "Tame Impala", summary: "Sounding a bit like Lennon, rocking a bit like Pink Floyd. It's a mix, and you don't want to forget it.", starting_year: '2012', genre: "psych-rock", tracks: ['It is Not Meant To Be', 'Desire Be Desire Go', 'Alter Ego', 'Lucidity', 'Why Wont You Make Up Your Mind?', 'Solitude Is Bliss', 'Jeremys Storm', 'Expectation', 'Bold Narrow Of Time', 'Runway Houses, Cities, And Clouds', 'I Dont Really Mind'] )
